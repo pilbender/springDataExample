771a860bb029e3a8b5794f19b9ce6e6c3376f387
Richard Scott Smith <scott.smith@isostech.com>
2014-08-27 17:25:59 -0700

    Fixed the Logger in PersonRepositoryOverrideImpl.
    Conflicts:
    	src/main/java/net/raescott/springdataexample/repository/PersonRepositoryOverrideImpl.java

--- src/main/java/net/raescott/springdataexample/repository/PersonRepositoryOverrideImpl.java
+++ src/main/java/net/raescott/springdataexample/repository/PersonRepositoryOverrideImpl.java
@@ -1,23 +1,28 @@
 package net.raescott.springdataexample.repository;
 
-import net.raescott.springdataexample.App;
 import net.raescott.springdataexample.Person;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Richard Scott Smith <scott.smith@isostech.com>
  */
 @Repository("personRepositoryOverride")
 public class PersonRepositoryOverrideImpl implements PersonRepositoryOverride {
+<<<<<<< HEAD
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
 	@PersistenceContext(unitName = "mysqlPersistenceUnit")
+=======
+	private static final Logger logger = LoggerFactory.getLogger(PersonRepositoryOverrideImpl.class);
+	@PersistenceContext(unitName = "h2PersistenceUnit")
+>>>>>>> 1784bf2... Fixed the Logger in PersonRepositoryOverrideImpl.
 	EntityManager em;
 
 	@Override
